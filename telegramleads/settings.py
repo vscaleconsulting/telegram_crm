528587dd99d9163a7d0954c2d0bb3593181fe366
NotVaishav <vaishavdhepe@gmail.com>
2021-06-07 14:43:46 +0530

    secret_key change

--- telegramleads/settings.py
+++ telegramleads/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
+
+environ.Env.read_env()
+
+DEBUG = env('DEBUG')
+
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d^&t24w5zwxd&2q&snqiq+te5u19^84ii+it6_-(#b3mltxw_b'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
