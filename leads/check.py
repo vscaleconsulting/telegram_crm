d3d93f1630fb2df3d0e4cd699b225725f83d0fbe
Gagandheep Raikar <32958460+Gagandheep@users.noreply.github.com>
2021-06-07 18:39:14 +0530

    update

--- leads/check.py
+++ leads/check.py
@@ -9,8 +9,8 @@ session_str = '1ApWapzMBuyhm1ynxKkQ4qL58doxHa27-g-aeN80l6kxSLbsPtaD5me6vNwXEXL5o
 # print(functions.get_session_str())
 # functions.update_usage_session_str(session_str)
 functions.add_session_str(phone_num, session_str)
-print(functions.add_all_users('XfceDevelopment'))
-functions.generate_message_campaign('cryptoinsiderslimited')
+# print(functions.add_all_users('XfceDevelopment'))
+# functions.generate_message_campaign('cryptoinsiderslimited')
 # print(functions.get_leads_to_msg(2, 'cryptoinsiderslimited'))
 
 # functions.check_grps()
