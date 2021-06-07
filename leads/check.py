3b59b2ae364e9651d9d8e2b4673d60e71fe4bd8e
NotVaishav <vaishavdhepe@gmail.com>
2021-06-07 16:04:07 +0530

    Update check.py

--- leads/check.py
+++ leads/check.py
@@ -8,8 +8,9 @@ session_str = '1ApWapzMBuyhm1ynxKkQ4qL58doxHa27-g-aeN80l6kxSLbsPtaD5me6vNwXEXL5o
 # functions.add_user_session(phone_num, session_str)
 # print(functions.get_session_str())
 # functions.update_usage_session_str(session_str)
+functions.add_session_str(phone_num, session_str)
 print(functions.add_all_users('XfceDevelopment'))
 functions.generate_message_campaign('cryptoinsiderslimited')
 # print(functions.get_leads_to_msg(2, 'cryptoinsiderslimited'))
-# functions.add_session_str(phone_num, session_str)
+
 # functions.check_grps()
