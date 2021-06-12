4b2357641c7293587c456ca41020affa0e2914c6
NotVaishav <vaishavdhepe@gmail.com>
2021-06-12 12:32:32 +0530

    made changes for deployment

--- leads/admin.py
+++ leads/admin.py
@@ -1,13 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 from .models import Agent, Lead, User, MessageCampaign, TGBot, TGSession, Category
 
 
+class MessageCampaignAdmin(admin.ModelAdmin):
+    actions = ['mark_replied']
+
+    @admin.action(description='Mark selected leads as replied')
+    def mark_replied(self, request, queryset):
+        updated = queryset.update(category='Replied')
+        self.message_user(request, ngettext(
+            '%d lead was successfully marked as replied.',
+            '%d leads were successfully marked as replied.',
+            updated,
+        ) % updated, messages.SUCCESS)
+        
+        
+
 # class TGBotAdmin(admin.ModelAdmin):
+
 
 admin.site.register(User)
 admin.site.register(Agent)
 admin.site.register(Lead)
-admin.site.register(MessageCampaign)
+admin.site.register(MessageCampaign, MessageCampaignAdmin)
 admin.site.register(TGBot)
 admin.site.register(TGSession)
-admin.site.register(Category)
+# admin.site.register(Category)
--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
-# python manage.py makemigrations
+python manage.py makemigrations
 
-# python manage.py migrate
+python manage.py migrate
 
 
 
 # python manage.py createsuperuser
 
-python manage.py shell < leads/check.py
+# python manage.py shell < leads/check.py
 
 
 python manage.py collectstatic --noinput
